13c8c77f1c9112e8d1f51c3854b0678b561e6fe1
Audiansyah <msaudiansyah@gmail.com>
2025-01-30 22:53:45 +0700

    v2

--- main.rs
+++ main.rs
@@ -1,8 +1,3 @@
-/*
-1. buka terminal
-2. jalankan perintah "cargo new nama-folder"
-3. buka folder di code editor */
-
 fn main() {
     println!("Hello, world!");
 }
@@ -89,4 +84,305 @@ fn number_convertion(){
     println!("{}", c);
 }
 
-//1:06:31
+// operator
+#[test]
+fn numeric_operator(){
+    let a = 10;
+    let b = 10;
+    let c = a * b;
+    println!("{}", c);
+    let d = a / b;
+    println!("{}", d);
+    let e = a + b;
+    println!("{}", e);
+}
+
+// augmented assignments
+#[test]
+fn augmented_assignment(){
+    let mut a = 10;
+    println!("{}", a);
+
+    a += 10;
+    println!("{}", a);
+
+    a -= 10;
+    println!("{}", a);
+}
+
+#[test]
+fn boolean(){
+    let a = true;
+    let b: bool = false;
+
+    println!("{} {}", a, b);
+}
+
+//comparison opertators >, <, <=, >=, ==
+#[test]
+fn  comparison(){
+    let a = 10;
+    let b = 20;
+    let result = a > b;
+
+    println!("{}", result);
+}
+
+// boolean operator &&(dan), ||(or), !(kebalikan)
+#[test]
+fn boolean_operator(){
+    let absen = 75;
+    let nilai_akhir = 80;
+    
+    let lulus_absen = absen >= 75;
+    let lulus_nilai_akhir = nilai_akhir >= 75;
+
+    let lulus = lulus_absen && lulus_nilai_akhir;
+    println!("{}", lulus);
+}
+
+//char
+#[test]
+fn  char(){
+    let char1 = 'a';
+    let char2 = 'b';
+
+    println!("{} {}", char1, char2);
+}
+
+//tuple: tipe dara kumpulan lebih dari satu tipe data dan jumlahnya sudah final(tidak bisa berkurang ataupun bertambah)
+#[test]
+fn  tuple(){
+    let mut data: (i32, f64, bool) = (10, 10.5, true);
+    println!("{:?}", data);
+    //akses tuple
+    // let a = data.0;
+    // let b = data.1;
+    // let c = data.2;
+
+    //destructuring
+    let(a, b, c) = data;
+    println!("{} {} {}", a, b, c);
+
+    //mutable
+    data.0 = 20;
+    data.1 = 20.5;
+    data.2 = false;
+    println!("{:?}", data);
+}
+
+fn unit(){
+    println!("hello");
+}
+
+//unit(tuple kosong)
+#[test]
+fn  test_unit(){
+    let result: () = unit();
+    println!("{:?}", result);
+
+    let test = ();
+    println!("{:?}", test);
+}
+
+//aray
+#[test]
+fn  array(){
+    let array = [1, 2, 3, 4, 5];
+    println!("{:?}", array);
+
+    //akses array
+    let a = array[0];
+    let b = array[1];
+    println!("{} {}", a, b, );
+}
+
+//mutable aray
+#[test]
+fn  mut_array(){
+    let mut array = [1, 2, 3, 4, 5];
+    println!("{:?}", array);
+
+    //akses array
+    let a = array[0];
+    let b = array[1];
+    println!("{} {}", a, b, );
+
+    array[0] = 10;
+    array[1] = 20;
+    println!("{:?}", array);
+
+    let length = array.len();
+    println!("{}", length);
+}
+
+//two_dimensional_array
+#[test]
+fn  two_dimensional_array(){
+    let matrix: [[i32; 3]; 2] = [
+        [1, 2, 3],
+        [4, 5, 6]
+    ];
+
+
+    //akses array
+    println!("{:?}", matrix);
+    println!("{:?}", matrix[0]);
+    println!("{:?}", matrix[1]);
+}
+
+//constant
+#[test]
+fn  coonstant(){
+    const MAXIMUM: i32 = 100;
+
+    println!("{}", MAXIMUM);
+}
+
+//variable scope
+#[test]
+fn  variable_scope(){
+    let audi = 1;
+
+    {
+        println!("inner audi: {}", audi);
+        let syawal = 2;
+        println!("inner syawal: {}", syawal);
+    }
+}
+
+/*garbage collection, stack and heap
+ pada prog-lang biasanya, garbage collection berguna untuk memantau data yang sudah tdak di gunakan di memory dengan menghapusnya secara otomatis.
+ 
+ sedangkan rust, membagi data di memory dalam dua bagian, yaitu stack and heap
+ - stack adalah bagian dimana data disimpan dalam struktur tumpukan "last in, first out", semua data di stack harus fixed size
+ - heap adalah seperti tempat untuk menyimpann data, dimana untuk tempat menyimpan data di heap kita akan melakukan request ke heap,
+   lalu didalam heap terdapat memory allocator yang bertugas untuk menemukan area kosong untuk menyimpan dan mengalokasikan data ke area tersebut.
+   seteah itu kita akan digeri pointer ke lokasi dimana data itu berada di heap.
+
+   pointer dari heap berukuran fix sized, oleh karena itu pointer tersebut akan di simpan di stack.
+  */ 
+#[test]
+fn  stack_heap(){
+    function_a();
+    function_b();
+}
+
+fn function_a(){
+    let a = 10;
+    let b = String::from("audi");
+
+    println!("{} {}", a, b);
+}
+fn function_b(){
+    let a = 10;
+    let b = String::from("syawal");
+
+    println!("{} {}", a, b);
+}
+
+/* drop function
+- adalah function untuk menghapus data, sehingga akan di bersihkan dari heap
+- saat variable keluar dari scopenya, yang artinya tidak bisa di akses lagi, secara otomatis rust akan  memanggil drop function
+- dan jika rust function() sudah selesai di eksekusi, maka function tersebut akan dihapus pula dari stack frame
+- oleh karena itu, rust tidak membutuhkan garbage collection ataupun manual memory management */
+
+/* &str dan string
+- rust memiliki type data text  yang fixed size, yaitu &str(string slice) dan yang bisa mengembang ukurannya yaitu string
+- &str karena ukurannya fixed size, jadi rust akan menyimpan di stack, sefangkan sting karena bisa mengembang, maka disimpan di heap 
+
+Immutable str
+- karena ukuran &str adalah fixed size, maka operasi &str adalah tipe data yang immutable, artinya isi data &str tidak bisa diubah
+- ketika kita buat mutable, dan mengubah data &str, sebenernya yang dilakukan adalah mengganti isi variablem bukan mengubah isi dari &str
+
+docs: https://doc.rust-lang.org/std/primitive.str.html */
+#[test]
+fn  string(){
+    let name: &str = "   Audiansyah   ";
+    let trim: &str = name.trim();
+
+    println!("{}", name);
+    println!("{}", trim);
+}
+
+#[test]
+fn  string_type(){
+    let mut name: String = String::from("audiansyah");
+    name.push_str(" Syawalhan");
+    println!("{}", name);
+
+    let audi = name.replace("audiansyah", "audi");
+    println!("{}", audi);
+}
+
+/*ownership
+adalah fitur yang digunakan oleh rust untuk menjadikan rust menjadi prog-lang yang aman dalam mengolah data di memory
+tanpa harus adanya fitur garbage collection atau manual memory management.
+untuk melakukan data management di memory, yang wajib di mengerti karena ini berdampak ke hampir semua fitur
+
+ownership rules:
+- sreiap value fi rust harus punya owner
+- dalam satu wakrtu, hanya boleh ada satu owner
+- ketika owner keluar scope, value akan dihapus
+*/
+
+#[test]
+fn ownership_rule() {
+    // a tidak bisa di akses disini, belum di deklarasikan
+    let a = 10; // a bisa mulai di akses
+
+    {// b tidak bisa diakses disini, karena belum di deklarasikan
+        let b = 20; // b bisa di akses mulai disini
+        println!("{}", b);
+    }//scope b selesai disini, b dihapus dan tidak bisa di akses lagi
+
+    println!("{}", a);
+}//scope a selesai disini, a dihapus dan tidak bisa di akses lagi
+
+/* data copy
+- sesuai aturan di own rule, setiap value harus dimiliki oleh satu ower pada satu waktu.
+- ketika kita berhasi dengan data maka akan dimiliki hanya oleh satu own.
+- semua data yg bersifat fixed size, ketika ditambahkan ke variable baru, akan memiliki data hasil copy dari variable lama
+- oleh karena itu, setiap data akan selalu dimiliki oleh satu own pada satu waktu
+*/
+#[test]
+fn data_copy() {
+    let a = 10;
+    let b = a; //copy data dari a ke b
+
+    println!("{} {}", a, b);
+}
+
+/*own movement
+- data copy tidak terjhadi untuk tipe data yang disimpan di heap
+- seperti aturan di ownership, dalam waktu satu waktu value hanya dimiliki satu own
+- maka kita coba buat variable baru dari variable lama, maka yang terjhadi bukanlan copy, melainkan transfer ownership dari own lama ke own baru
+- setelah proses transfer selesai, secara otomats own lama akan dianggap tidak valid lagi digunakan
+ */
+
+#[test]
+fn ownership_movement() {
+    let name1 = String::from("audi");
+
+    //ownership dari name1 dipindahkan ke name1
+    let name2 = name1;
+
+    println!("{}", name2);
+}
+
+/*clone
+- sekarang kita tahu bahwa data di stack akan di copy sedangkan data di heap akan di pindahkan ownershipnya
+- lantas bagaimana jika kita juga ingin melaklukan copy untuk data di heap?
+- maka kita harus melakukan clone
+- clone adalah membuat data tiruan yang sama dari data aslinya
+- string memiliki method clone() untuk melakukan ini
+- saat kita memanggil method clone() maka method tersebut akan meng-copy data string menjadi data string baru
+- semua tipe data yang di simpan di heap di rust memiliki method clone()
+ */
+#[test]
+fn clone() {
+    let name1 = String::from("audi");
+    let name2 = name1.clone();
+
+    println!("{} {}", name1, name2);
+}
